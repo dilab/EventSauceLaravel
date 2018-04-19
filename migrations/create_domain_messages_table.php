09d4d8a5bd7bdf2d1a3279f2fbdb0a0cb20450ba
xu ding <thedilab@gmail.com>
2018-04-19 11:50:45 +0800

    add migrations

---
+++ migrations/create_domain_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDomainMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domain_messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('event_id', 36);
+            $table->string('event_type', 100);
+            $table->string('aggregate_root_id', 36)->nullable()->index();
+            $table->dateTime('time_of_recording', 6)->index();
+            $table->text('payload');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domain_messages');
+    }
+}
---
+++ src/EventSauceServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Spatie\Backup;
+
+use Illuminate\Support\ServiceProvider;
+use Spatie\Backup\Commands\ListCommand;
+use Spatie\Backup\Helpers\ConsoleOutput;
+use Spatie\Backup\Commands\BackupCommand;
+use Spatie\Backup\Commands\CleanupCommand;
+use Spatie\Backup\Commands\MonitorCommand;
+use Spatie\Backup\Notifications\EventHandler;
+
+class EventSauceServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/event_sauce.php' => config_path('event_sauce.php'),
+        ], 'config');
+
+        $this->loadMigrationsFrom(__DIR__.'/../migrations');
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/event_sauce.php', 'event_sauce');
+
+//        $this->app['events']->subscribe(EventHandler::class);
+//
+//        $this->app->bind('command.backup:run', BackupCommand::class);
+//        $this->app->bind('command.backup:clean', CleanupCommand::class);
+//        $this->app->bind('command.backup:list', ListCommand::class);
+//        $this->app->bind('command.backup:monitor', MonitorCommand::class);
+//
+//        $this->commands([
+//            'command.backup:run',
+//            'command.backup:clean',
+//            'command.backup:list',
+//            'command.backup:monitor',
+//        ]);
+
+        $this->app->singleton(ConsoleOutput::class);
+    }
+}
