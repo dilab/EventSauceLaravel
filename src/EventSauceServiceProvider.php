223cffa98c9275959e727bdd06e788d6b20eaf21
xu ding <thedilab@gmail.com>
2018-04-20 12:57:25 +0800

    cleanup sync

--- src/EventSauceServiceProvider.php
+++ src/EventSauceServiceProvider.php
@@ -30,21 +30,5 @@ class EventSauceServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/event_sauce.php', 'event_sauce');
-
-//        $this->app['events']->subscribe(EventHandler::class);
-//
-//        $this->app->bind('command.backup:run', BackupCommand::class);
-//        $this->app->bind('command.backup:clean', CleanupCommand::class);
-//        $this->app->bind('command.backup:list', ListCommand::class);
-//        $this->app->bind('command.backup:monitor', MonitorCommand::class);
-//
-//        $this->commands([
-//            'command.backup:run',
-//            'command.backup:clean',
-//            'command.backup:list',
-//            'command.backup:monitor',
-//        ]);
-
-        $this->app->singleton(ConsoleOutput::class);
     }
 }
